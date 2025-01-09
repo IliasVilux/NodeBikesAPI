26f52b8c80be0bb1ee38bd7d06e3d3cf9f3983fb
IliasVilux <iliasotsman@gmail.com>
2025-01-09 17:13:51 +0100

    feat: :sparkles: Fetch data from local in frontend

--- bikes-store/src/views/HomeView.vue
+++ bikes-store/src/views/HomeView.vue
@@ -1,5 +1,33 @@
 <script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+import { onMounted } from 'vue'
+
+const bikes = ref([])
+const loading = ref(false)
+const error = ref(null)
+
+const fetchData = async () => {
+  loading.value = true
+  try {
+    const response = await axios.get('http://localhost:3000/motos')
+    bikes.value = response.data
+  } catch (err) {
+    error.value = err
+  } finally {
+    loading.value = false
+  }
+}
+
+onMounted(fetchData)
 </script>
 
 <template>
+    <div v-if="loading">Loading...</div>
+    <div v-else-if="error">Error: {{ error.message }}</div>
+    <ul v-else>
+    <li v-for="bike in bikes" :key="bike.id">
+        {{ bike }}
+    </li>
+    </ul>
 </template>
