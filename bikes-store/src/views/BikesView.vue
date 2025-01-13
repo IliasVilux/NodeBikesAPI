516092a82126928881c735b2be417505e6e5d2b3
IliasVilux <iliasotsman@gmail.com>
2025-01-13 13:14:04 +0100

    feat: :construction: Initial frontend 'Views' & 'Components' structure v1

---
+++ bikes-store/src/views/BikesView.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+import { onMounted } from 'vue'
+import { RouterLink } from 'vue-router'
+
+const bikes = ref([])
+const loading = ref(false)
+const error = ref(null)
+
+const fetchData = async () => {
+  loading.value = true
+  try {
+    const response = await axios.get(`${import.meta.env.VITE_API_URL}/motos`)
+    bikes.value = response.data
+  } catch (err) {
+    error.value = err
+  } finally {
+    loading.value = false
+  }
+}
+onMounted(fetchData)
+</script>
+
+<template>
+  <div v-if="loading">Loading...</div>
+  <div v-else-if="error">Error: {{ error.message }}</div>
+  <div v-else>
+    <ul>
+      <li v-for="bike in bikes" :key="bike.id">
+        <RouterLink :to="{ name: 'detail', params: { id: bike.id } }">
+          {{ bike.name }}
+        </RouterLink>
+      </li>
+    </ul>
+  </div>
+</template>
--- bikes-store/src/views/HomeView.vue
+++ bikes-store/src/views/HomeView.vue
@@ -1,37 +1,7 @@
 <script setup>
-import { ref } from 'vue'
-import axios from 'axios'
-import { onMounted } from 'vue'
-import { RouterLink } from 'vue-router'
-
-const bikes = ref([])
-const loading = ref(false)
-const error = ref(null)
-
-const fetchData = async () => {
-  loading.value = true
-  try {
-    const response = await axios.get(`${import.meta.env.VITE_API_URL}/motos`)
-    bikes.value = response.data
-  } catch (err) {
-    error.value = err
-  } finally {
-    loading.value = false
-  }
-}
-onMounted(fetchData)
+import HelloWorld from '@/components/HelloWorld.vue';
 </script>
 
 <template>
-  <div v-if="loading">Loading...</div>
-  <div v-else-if="error">Error: {{ error.message }}</div>
-  <div v-else>
-    <ul>
-      <li v-for="bike in bikes" :key="bike.id">
-        <RouterLink :to="{ name: 'detail', params: { id: bike.id } }">
-          {{ bike.name }}
-        </RouterLink>
-      </li>
-    </ul>
-  </div>
+  <h1>Home View</h1>
 </template>
