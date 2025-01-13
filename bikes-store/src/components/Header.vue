8d60bb0d9e0885604e8f425dd2e85af0edd0e2c3
IliasVilux <iliasotsman@gmail.com>
2025-01-14 00:30:11 +0100

    feat: :construction: 'Header' structure and create 'SearchBar' component

--- bikes-store/src/components/Header.vue
+++ bikes-store/src/components/Header.vue
@@ -1,3 +1,13 @@
-<script setup></script>
+<script setup>
+import Svg from '@/components/Svg.vue'
+import SearchBar from '@/components/SearchBar.vue';
+</script>
 
-<template></template>
+<template>
+    <header class="w-full bg-white">
+        <nav class="max-w-4xl mx-auto flex items-center justify-between py-2 px-2 md:px-0">
+            <Svg name="logo" />
+            <SearchBar />
+        </nav>
+    </header>
+</template>
