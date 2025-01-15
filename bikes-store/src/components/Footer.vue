c89fe9f2e5db726652f8c80d67083506e1229a40
IliasVilux <iliasotsman@gmail.com>
2025-01-15 21:28:07 +0100

    feat: :sparkles: Implement 'Footer'

--- bikes-store/src/components/Footer.vue
+++ bikes-store/src/components/Footer.vue
@@ -1,3 +1,11 @@
-<script setup></script>
+<script setup>
+import Svg from '@/components/Svg.vue'
+</script>
 
-<template></template>
+<template>
+    <footer class="w-full bg-white py-3 flex justify-center mt-10">
+        <a href="https://github.com/IliasVilux/NodeBikesAPI" target="_blank">
+            <Svg name="github" class="size-7 text-accent" />
+        </a>
+    </footer>
+</template>
