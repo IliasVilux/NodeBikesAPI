9e4aed7d1a72a8066284e91d92d527a66391f583
IliasVilux <iliasotsman@gmail.com>
2025-01-31 00:33:42 +0100

    fix: :bug: Remove temporaly page background

--- bikes-store/src/App.vue
+++ bikes-store/src/App.vue
@@ -3,8 +3,5 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div
-    class="absolute inset-0 -z-10 h-full w-full bg-background [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#DBEAFE_100%)]"
-  ></div>
   <RouterView />
 </template>
