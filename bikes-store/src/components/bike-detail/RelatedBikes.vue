7975ee368a833bf1a13624241fcb4f70647b90fe
IliasVilux <iliasotsman@gmail.com>
2025-04-13 16:39:08 +0200

    Format

--- bikes-store/src/components/bike-detail/RelatedBikes.vue
+++ bikes-store/src/components/bike-detail/RelatedBikes.vue
@@ -6,8 +6,8 @@ import BikeCard from '@/components/common/BikeCard.vue'
 const props = defineProps({
   bikeId: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
 })
 const relatedBikes = ref([])
 
@@ -29,10 +29,10 @@ onMounted(() => {
 
 <template>
   <div v-if="relatedBikes.length > 0" class="max-w-screen-xl mx-auto px-4">
-      <div class="mt-28">
-        <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
-          <BikeCard v-for="bike in relatedBikes" :key="bike.id" :bike="bike" />
-        </div>
+    <div class="mt-28">
+      <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+        <BikeCard v-for="bike in relatedBikes" :key="bike.id" :bike="bike" />
       </div>
     </div>
+  </div>
 </template>
